578e2e78f8c3f1c767bccc7a2ce5bdedcc1202dd
Manhoni <jaomanhoni@gmail.com>
2026-01-10 15:55:18 -0300

    Ajuste gitignore

--- Infrastructure/NFSe/Abrasf/Certificates/ICertificateProvider.cs
+++
@@ -1,8 +0,0 @@
-using System.Security.Cryptography.X509Certificates;
-
-namespace MGMBlazor.Infrastructure.NFSe.Certificates;
-
-public interface ICertificateProvider
-{
-    X509Certificate2 ObterCertificado();
-}
